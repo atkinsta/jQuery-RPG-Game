15704ad30e1b2323b8ffec279ded7000619f9826
atkinsta <atkinstaa@gmail.com>
2018-06-07 12:45:59 -0400

    Worked on sessionStorage for variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,6 @@
+// json stringify
+// json parse
+
 //Animating the sprites when they attack, have these set up to load with the page because 1. it looks cool, and 2. need to have them toggled off at start. 
 $("#skeleton").sprite({
     fps: 10,
@@ -34,53 +37,82 @@ $("#rogue").sprite({
 
 //Game objects go here
 var skeleton = {
+    id: "skeleton",
     maxHealth: 500,
-    currentHealth: 500,
-    damage: Math.floor([Math.random() * 100]+ 20),
+    damageTaken: parseInt(sessionStorage.getItem("sDamageTaken")),
+    currentHealth: this.maxHealth - this.damageTaken,
+    damage: parseInt(sessionStorage.getItem("sDamage")),
     attack: function () {
         $("#skeleton").spToggle();
     }
 };
 
 var adventurer = {
+    id: "adventurer",
     maxHealth: 500,
-    currentHealth: 500,
+    damageTaken: parseInt(sessionStorage.getItem("aDamageTaken")),
+    currentHealth: this.maxHealth - this.damageTaken,
     attack: function () {
         $("#adventurer").spToggle();
     }
 };
 
 var rogue = {
+    id: "rogue",
     maxHealth: 450,
-    currentHealth: 450,
+    damageTaken: parseInt(sessionStorage.getItem("rDamageTaken")),
+    currentHealth: this.maxHealth - this.damageTaken,
     damage: Math.floor(Math.random() * 130),
     attack: function () {
         $("#rogue").spToggle();
     }
 };
 
+var bandit = {
+    id: "bandit",
+    maxHealth: 450,
+    damageTaken: parseInt(sessionStorage.getItem("bDamageTaken")),
+    currentHealth: this.maxHealth - this.damageTaken,
+    damage: Math.floor(Math.random() * 130),
+    attack: function () {
+        $("#bandit").spToggle();
+    }
+};
+
 var player = {
-    character: skeleton,
-    currentEnemy: adventurer
+    character: eval(sessionStorage.getItem("pCharacter")),
+    currentEnemy: eval(sessionStorage.getItem("pEnemy")),
 };
 
 // On click section, have most event handlers here. 
-$("#attackbutton").on("click", function () { //Side note, I was blown away this works. So many references to keys in other objects. 
-    player.character.attack();
-    player.currentEnemy.currentHealth -= player.character.damage;
-    $("#ehealth").attr("style", "width:" + (player.currentEnemy.currentHealth/player.currentEnemy.maxHealth) * 100 + "%;"); //TODO: Replace with a screen display function. 
-    console.log(adventurer.currentHealth);
-})
-$(".characteroption").on("mouseover", function () {
-    $('[data-toggle="popover"]').popover();
-});
+window.onload = function () {
+    $("#attackbutton").on("click", function () { //Side note, I was blown away this works. So many references to keys in other objects. 
+        player.character.attack();
+        player.currentEnemy.currentHealth -= player.character.damage;
+        $("#ehealth").attr("style", "width:" + (player.currentEnemy.currentHealth / player.currentEnemy.maxHealth) * 100 + "%;"); //TODO: Replace with a screen display function. 
+        console.log(adventurer.currentHealth);
+    });
+    $(".characteroption").on("mouseover", function () {
+        $('[data-toggle="popover"]').popover();
+    });
 
-$(".characteroption").on("click", function () {         // Logs the players character choice and moves them onto the challenge section.
-    player.character = $(this).attr("value");           // $(this) refers to the value of the specific .characteroption we clicked on.
-    console.log(player.character);
-    window.location.href = "challenge.html";
-})
+    $(".characteroption").on("click", function () {         // Logs the players character choice and moves them onto the challenge section.
+        sessionStorage.setItem("pCharacter", $(this).attr("value"))         // $(this) refers to the value of the specific .characteroption we clicked on.
+        console.log(sessionStorage.getItem("pCharacter"));
+        window.location.href = "challenge.html";
+    });
 
-$("#startbutton").on("click", function () {
-    window.location.href = "characterselect.html";
-})
+    $("#startbutton").on("click", function () {
+        window.location.href = "characterselect.html";
+    });
+
+    $(".challenger").on("click", function () {
+        sessionStorage.setItem("pEnemy", $(this).attr("value"));
+        window.location.href = "fight.html";
+    });
+    $("#beginfight").on("click", function () {
+        $(".playerid").attr("id", player.character.id);
+        $(".enemyid").attr("id", player.currentEnemy.id);
+    });
+}
+
