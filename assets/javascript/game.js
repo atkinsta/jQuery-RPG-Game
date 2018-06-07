af104f5685e9f8211654e436a28e0ae36afb4ee1
atkinsta <atkinstaa@gmail.com>
2018-06-07 14:29:02 -0400

    Had to rework lots of objects and functions to cope with sessionStorage

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -22,7 +22,7 @@ $("#bandit").sprite({
     fps: 10,
     no_of_frames: 8,
     on_last_frame: function (obj) {
-        obj.spStop();
+        obj.spToggle();
     }
 });
 
@@ -30,7 +30,7 @@ $("#rogue").sprite({
     fps: 10,
     no_of_frames: 10,
     on_last_frame: function (obj) {
-        obj.spStop();
+        obj.spToggle();
     }
 });
 
@@ -39,12 +39,10 @@ $("#rogue").sprite({
 var skeleton = {
     id: "skeleton",
     maxHealth: 500,
-    damageTaken: parseInt(sessionStorage.getItem("sDamageTaken")),
+    damageTaken: 0,
     currentHealth: this.maxHealth - this.damageTaken,
-    damage: parseInt(sessionStorage.getItem("sDamage")),
-    attack: function () {
-        $("#skeleton").spToggle();
-    }
+    healthPercentage: (this.currentHealth/this.maxHealth) * 100,
+    damage: 10,
 };
 
 var adventurer = {
@@ -52,9 +50,8 @@ var adventurer = {
     maxHealth: 500,
     damageTaken: parseInt(sessionStorage.getItem("aDamageTaken")),
     currentHealth: this.maxHealth - this.damageTaken,
-    attack: function () {
-        $("#adventurer").spToggle();
-    }
+    healthPercentage: (this.currentHealth/this.maxHealth) * 100,
+    damage: 10,
 };
 
 var rogue = {
@@ -62,43 +59,50 @@ var rogue = {
     maxHealth: 450,
     damageTaken: parseInt(sessionStorage.getItem("rDamageTaken")),
     currentHealth: this.maxHealth - this.damageTaken,
-    damage: Math.floor(Math.random() * 130),
-    attack: function () {
-        $("#rogue").spToggle();
-    }
+    healthPercentage: (this.currentHealth/this.maxHealth) * 100,
+    damage: 10,
 };
 
 var bandit = {
     id: "bandit",
     maxHealth: 450,
-    damageTaken: parseInt(sessionStorage.getItem("bDamageTaken")),
+    damageTaken: 50,
     currentHealth: this.maxHealth - this.damageTaken,
-    damage: Math.floor(Math.random() * 130),
-    attack: function () {
-        $("#bandit").spToggle();
-    }
+    healthPercentage: (this.currentHealth/this.maxHealth) * 100,
+    damage: 10,
 };
 
 var player = {
-    character: eval(sessionStorage.getItem("pCharacter")),
-    currentEnemy: eval(sessionStorage.getItem("pEnemy")),
+    character: JSON.parse(sessionStorage.getItem("pCharacter")), 
+    currentEnemy: JSON.parse(sessionStorage.getItem("eCharacter"))
 };
+
+function attack() {
+    
+}
 
 // On click section, have most event handlers here. 
 window.onload = function () {
     $("#attackbutton").on("click", function () { //Side note, I was blown away this works. So many references to keys in other objects. 
-        player.character.attack();
-        player.currentEnemy.currentHealth -= player.character.damage;
-        $("#ehealth").attr("style", "width:" + (player.currentEnemy.currentHealth / player.currentEnemy.maxHealth) * 100 + "%;"); //TODO: Replace with a screen display function. 
+        console.log(player.currentEnemy.damageTaken);
+        $("#ehealth").attr("style", "width:" + player.currentEnemy.healthPercentage + "%;"); //TODO: Replace with a screen display function. 
         console.log(adventurer.currentHealth);
     });
+
     $(".characteroption").on("mouseover", function () {
         $('[data-toggle="popover"]').popover();
     });
 
     $(".characteroption").on("click", function () {         // Logs the players character choice and moves them onto the challenge section.
-        sessionStorage.setItem("pCharacter", $(this).attr("value"))         // $(this) refers to the value of the specific .characteroption we clicked on.
-        console.log(sessionStorage.getItem("pCharacter"));
+        var pChoice = $(this).attr("value");         // $(this) refers to the value of the specific .characteroption we clicked on.
+        if (pChoice === "adventurer")
+            sessionStorage.setItem("pCharacter", JSON.stringify(adventurer));
+        else if (pChoice === "skeleton")
+            sessionStorage.setItem("pCharacter", JSON.stringify(skeleton));
+        else if (pChoice === "bandit")
+            sessionStorage.setItem("pCharacter", JSON.stringify(bandit));
+        else if (pChoice === "rogue")
+            sessionStorage.setItem("pcharacter", JSON.stringify(rogue));
         window.location.href = "challenge.html";
     });
 
@@ -107,12 +111,19 @@ window.onload = function () {
     });
 
     $(".challenger").on("click", function () {
-        sessionStorage.setItem("pEnemy", $(this).attr("value"));
+        var challenged = $(this).attr("value");
+        if (challenged === "adventurer")
+            sessionStorage.setItem("eCharacter", JSON.stringify(adventurer));
+        else if (challenged === "bandit")
+            sessionStorage.setItem("eCharacter", JSON.stringify(bandit));
+        else if (challenged === "skeleton")
+            sessionStorage.setItem("eCharacter", JSON.stringify(skeleton));
+        else if (challenged === "rogue")
+            sessionStorage.setItem("eCharacter", JSON.stringify(rogue));
         window.location.href = "fight.html";
     });
-    $("#beginfight").on("click", function () {
-        $(".playerid").attr("id", player.character.id);
-        $(".enemyid").attr("id", player.currentEnemy.id);
-    });
+
+    $(".playerid").attr("id", player.character.id);
+    $(".enemyid").attr("id", player.currentEnemy.id);
 }
 
